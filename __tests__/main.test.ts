0c16931a37f04d2746d6bde59e1e535be90c7bc8
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-01-08 10:33:23 +0800

    feat(): :sparkles: 文件及文件夹合并

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,25 +1,68 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import {
+  isFileAndDirValid,
+  formatFileAndDirValue
+} from '../src/file-and-dir-valid'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('input type not valid', async () => {
+  const input = ''
+  expect(isFileAndDirValid(input)).toBeFalsy()
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('test runs and need not merge', () => {
+  process.env['INPUT_REMOTE_PATH'] = path.join(__dirname, 'temp-path', 'test')
+  process.env['INPUT_REMOTE_PATH_PREV'] = path.join(
+    __dirname,
+    'temp-path',
+    'test-prev'
+  )
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
+
+test('test runs and need merge but merge_file_dir is not valid', () => {
+  process.env['INPUT_REMOTE_PATH'] = path.join(__dirname, 'temp-path', 'test')
+  process.env['INPUT_REMOTE_PATH_PREV'] = path.join(
+    __dirname,
+    'temp-path',
+    'test-prev'
+  )
+  process.env['INPUT_NEED_MERGE'] = 'true'
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
+
+test('test runs and need merge and success', () => {
+  process.env['INPUT_REMOTE_PATH'] = path.join(
+    __dirname,
+    '..',
+    'temp-path',
+    'test'
+  )
+  process.env['INPUT_REMOTE_PATH_PREV'] = path.join(
+    __dirname,
+    '..',
+    'temp-path',
+    'test-prev'
+  )
+  process.env['INPUT_NEED_MERGE'] = 'true'
+  process.env['INPUT_MERGE_FILE_DIR'] = './.gitignore,./.github'
+  process.env['INPUT_DIST_PATH_NAME'] = 'temp-path'
+
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,36 @@
 import * as core from '@actions/core'
 import * as fsExtra from 'fs-extra'
+import * as path from 'path'
 import {formatFileAndDirValue, isFileAndDirValid} from './file-and-dir-valid'
 
 async function run(): Promise<void> {
   try {
-    const need_merge: string = core.getInput('need_merge')
+    const need_merge: string = core.getInput('need_merge') || 'false'
     const merge_file_dir: string = core.getInput('merge_file_dir')
-    const dist_path_name: string = core.getInput('dist_path_name')
+    const dist_path_name: string = core.getInput('dist_path_name') || 'dist'
 
-    if (!need_merge) {
+    if (need_merge === 'false') {
       core.info('need not merge file and dir')
     } else if (!isFileAndDirValid(merge_file_dir)) {
       core.info('merge_file_dir is not valid')
     } else {
+      const distPath = path.join(__dirname, '..', dist_path_name)
+      if (!fsExtra.existsSync(distPath)) fsExtra.mkdirSync(distPath)
+
       const fileAndPathList = formatFileAndDirValue(merge_file_dir)
-      fileAndPathList.forEach(targetPath => {
-        fsExtra.copySync(targetPath, `./${dist_path_name}/${targetPath}`)
-        core.info(`mv file or dir to ${dist_path_name}`)
-      })
+
+      for (const targetPath of fileAndPathList) {
+        const copyPath = path.join(distPath, targetPath)
+        const originPath = path.join(__dirname, '..', targetPath)
+        if (fsExtra.existsSync(originPath)) {
+          fsExtra.copySync(originPath, copyPath, {
+            overwrite: true
+          })
+          core.debug(`merge ${originPath} to ${copyPath}`)
+        } else {
+          core.debug(`the file or dir path ${originPath} is not exists`)
+        }
+      }
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
